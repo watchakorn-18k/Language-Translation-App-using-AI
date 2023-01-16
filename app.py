71ba548ef0822fa8079026d69ff009cf26b3ee86
watchakorn-18k <porton2559@gmail.com>
2023-01-17 06:58:52 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,112 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "แอพแปลภาษาด้วย CHATGPT"
+    window_width, window_height = (400, 725)
+    page.window_width = window_width
+    page.window_height = window_height
+    page.window_always_on_top = True
+    page.window_skip_task_bar = True
+    input_text = ft.TextField(width=200)
+    pr = ft.ProgressBar(width=200)
+    pr.value = 0
+
+    def translating(e):
+        data = input_text.value
+        if data != "":
+            pr.value = None
+            page.update()
+            import openai
+            import re
+
+            # เข้ารหัส API key
+            openai.api_key = "API key for the CHATGPT"
+
+            model_engine = "text-davinci-003"
+
+            # Generate a response
+            completion = openai.Completion.create(
+                engine=model_engine,
+                prompt=f"{data} translate to write in words english",
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+
+            response = completion.choices[0].text
+            output = re.sub(r"^\s+|\s+$", "", response)
+            from googletrans import Translator
+
+            translator = Translator()
+            pr.value = 0
+            page.update()
+            result.value = output
+            page.update()
+            result2.value = translator.translate(src="en", dest="th", text=output).text
+            page.update()
+        else:
+            result.value = "Fill in the text box"
+            result2.value = "กรอกข้อความลงในช่องป้อนข้อมูล"
+            page.update()
+
+    cal_btn = ft.ElevatedButton(text="แปลภาษา", width=200, on_click=translating)
+    result = ft.Text("", width=150)
+    result2 = ft.Text("", width=150)
+
+    def copying(e):
+        page.set_clipboard(result.value)
+        page.snack_bar = ft.SnackBar(
+            content=ft.Text("คัดลอกแล้ว"),
+        )
+        page.snack_bar.open = True
+        page.update()
+
+    copy_btn = ft.IconButton(icon=ft.icons.COPY, on_click=copying)
+    content = ft.Row(
+        [
+            result,
+            copy_btn,
+        ],
+        alignment=ft.MainAxisAlignment.END,
+    )
+
+    def copying2(e):
+        page.set_clipboard(result2.value)
+        page.snack_bar = ft.SnackBar(
+            content=ft.Text("คัดลอกแล้ว"),
+        )
+        page.snack_bar.open = True
+        page.update()
+
+    copy_btn2 = ft.IconButton(icon=ft.icons.COPY, on_click=copying2)
+    content2 = ft.Row(
+        [
+            result2,
+            copy_btn2,
+        ],
+        alignment=ft.MainAxisAlignment.END,
+    )
+
+    page.add(
+        ft.Row(
+            [
+                ft.Column(
+                    [
+                        ft.Text(value="แอพแปลภาษาด้วย CHATGPT", text_align="center"),
+                        input_text,
+                        cal_btn,
+                        pr,
+                        content,
+                        content2,
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+
+ft.app(target=main)
---
+++ readme.md
@@ -0,0 +1,30 @@
+<p align="center"><img src="src\icon.ico" width=50></p>
+
+# Language Translation App using CHATGPT
+
+This app utilizes the power of the CHATGPT language model to provide real-time translation of text input in Thai and English.
+
+- Support for Thai and English languages
+- Easy-to-use interface
+
+## How to use
+
+1. Enter text in any language in the text field.
+2. Press the แปลภาษา button to get the translated text
+
+## Installation
+
+1. Make sure you have Python 3 installed on your system
+2. Clone the repository or download the zip file
+3. Run the command `pip install -r requirements.txt` to install the necessary dependencies
+4. Run the app with the command `python app.py`
+
+## Development
+
+This app is open-source and can be further developed. Feel free to make pull requests or suggest new features.
+
+## Technical details
+
+The app is built using the OpenAI's CHATGPT model, which is a powerful language generation model. It uses the Hugging Face's transformers library to fine-tune the model and make it work for our specific use case.
+
+**IMPORTANT**: Remember to enter your API key for the CHATGPT model before running the app in line 24
