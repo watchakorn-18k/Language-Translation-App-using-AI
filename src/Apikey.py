7f4306d176e1ce228102768b347ec79e0b86ce05
watchakorn-18k <wk18k@proton.me>
2024-10-01 13:07:35 +0700

    fix : add defualt model in json file

--- src/Apikey.py
+++ src/Apikey.py
@@ -4,7 +4,7 @@ import json
 
 class ApikeyGPT:
     def __init__(self) -> None:
-        self.data = {"api_key": "API key for the CHATGPT"}
+        self.data = {"api_key": "API key for the CHATGPT", "model": "gpt-4o"}
 
     def create_file_json(self):
         with open("api_key_chatgpt.json", "w") as outfile:
